b6bf9aa1e827c74a09cc22c9367b395a0a0f6335
florian.charpentier <florian.charpentier@epita.fr>
2021-06-05 17:13:50 +0200

    added default channle config

--- db/init.py
+++ db/init.py
@@ -13,6 +13,10 @@ admin_name = input("Enter the application administrator username: ")
 admin_email = input("Enter the application administrator email: ")
 admin_password = getpass("Enter the application administrator password: ")
 admin_hashed_password = bcrypt(admin_password, 14).decode()
+smtp_host = input("Enter the SMTP host to use when sending automated mails: ")
+smtp_port = input("Enter the SMTP port: ")
+sender_email = input("Enter the email address to use when sending automated mails: ")
+sender_password = input("Enter the SMTP password")
 
 tmp_file = "db/_init_db_tmp.js"
 with open(tmp_file, "w") as f:
@@ -20,6 +24,10 @@ with open(tmp_file, "w") as f:
     script_js = script_js.replace("{admin_name}", admin_name)
     script_js = script_js.replace("{admin_email}", admin_email)
     script_js = script_js.replace("{admin_hashed_password}", admin_hashed_password)
+    script_js = script_js.replace("{smtp_host}", smtp_host)
+    script_js = script_js.replace("{smtp_port}", smtp_port)
+    script_js = script_js.replace("{sender_email}", sender_email)
+    script_js = script_js.replace("{sender_password}", sender_password)
     f.write(script_js)
 
 
