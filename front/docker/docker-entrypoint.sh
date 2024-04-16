4b74593ea2cd754f13b73c6c62c04d850026c3e6
AnnaOquidam <oquidam@et.esiea.fr>
2024-04-16 11:52:01 +0200

    Docker-compose functionnal

--- front/docker/docker-entrypoint.sh
+++ front/docker/docker-entrypoint.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 set -e
 
-if [[ "$1" =~ "dev" ]]; then
-    if [[ ! -f ".node_installed" ]]; then
-      echo ".node_installed not found, installing node modules"
-      rm -rf node_modules
-      npm install
-      touch .node_installed
-    fi
-    npm run serve
-fi
-
-if [[ "$1" =~ "shell" ]]; then
-    /bin/bash
-fi
-
-exec "$@"
+npm run serve
