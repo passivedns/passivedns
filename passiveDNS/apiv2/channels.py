47dd3c0f23616b42a846788df30c7855c0fc93ec
AnnaOquidam <oquidam@et.esiea.fr>
2024-05-14 15:24:17 +0200

    scheduler_admin and channels controller

---
+++ passiveDNS/apiv2/channels.py
@@ -0,0 +1,47 @@
+from fastapi import APIRouter, Depends, HTTPException
+
+from apiv2.auth import get_current_user
+from models.channel import Channel
+from models.user import User
+from models.user_channel import UserChannel
+from db.database import ObjectNotFound
+
+channels_router = APIRouter()
+
+
+# get the list of all available channels
+@channels_router.get("/channels")
+def channels_list(user: User=Depends(get_current_user)):
+    ch_list = Channel.list()
+
+    username = user.username
+    user_ch_list = UserChannel.list_from_username(username)
+
+    out = []
+    for ch in ch_list:
+        found = False
+        for u_ch in user_ch_list:
+            if u_ch.channel_name == ch.name:
+                found = True
+                break
+        if not found:
+            out.append(ch)
+
+    return {
+        "msg": "available channel list retrieved",
+        "channel_list": [
+            ch.safe_json() for ch in ch_list
+        ]
+    }
+
+
+@channels_router.get("/channels/{channel_name}")
+def channel_get(channel_name):
+    try:
+        ch = Channel.get(channel_name)
+        return {
+            "msg": f"channel {ch.name} retrieved",
+            "channel": ch.safe_json()
+        }
+    except ObjectNotFound:
+        raise HTTPException(status_code=404, detail="channel not found")
---
+++ passiveDNS/apiv2/scheduler_admin.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, HTTPException, Depends
+from pydantic import BaseModel
+
+from apiv2.auth import get_current_user
+from models.user import User
+
+scheduler_admin_router = APIRouter()
+
+class PasswordJson(BaseModel):
+    password: str
+
+@scheduler_admin_router.post("/admin/scheduler/{scheduler_name}")
+def create_scheduler(scheduler_name, data: PasswordJson, admin_user: User=Depends(get_current_user)):
+    if User.exists(scheduler_name):
+        raise HTTPException(status_code=500, detail="name unavailable")
+
+    new_scheduler = User.new(scheduler_name, data.password, admin_user.email, is_scheduler=True)
+    new_scheduler.insert()
+
+    return {
+        "msg": "scheduler user created",
+        "scheduler": new_scheduler.safe_json()
+    }
+
+@scheduler_admin_router.put("/admin/scheduler/{scheduler_name}")
+def update_scheduler(scheduler_name, data: PasswordJson):
+    if not User.exists(scheduler_name):
+        raise HTTPException(status_code=404, detail="scheduler not found")
+
+    scheduler = User.get(scheduler_name)
+    scheduler.update_password(data.password)
+
+    return {
+        "msg": "scheduler user updated",
+        "scheduler": scheduler.safe_json()
+    }
