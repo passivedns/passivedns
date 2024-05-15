3138f8decbf2c483995f443e95f3dc9df81e72e4
AnnaOquidam <oquidam@et.esiea.fr>
2024-05-15 13:40:49 +0200

    channel_admin and user_channel controllers

--- passiveDNS/main.py
+++ passiveDNS/main.py
@@ -10,7 +10,7 @@ config.init_config()
 from apiv2.alert import alert_router
 from apiv2.auth import auth_router, get_current_user, check_admin_role, check_admin_user_role, check_scheduler_role
 from apiv2.channels import channels_router
-from api.channels_admin import channels_admin_router
+from apiv2.channels_admin import channels_admin_router
 from apiv2.domain_name import domain_name_router
 #from api.infos import infos_router
 from apiv2.resolution import resolution_router
@@ -18,7 +18,7 @@ from apiv2.scheduler import scheduler_router
 from apiv2.scheduler_admin import scheduler_admin_router
 #from api.tag import tag_router
 #from api.tag_dn_ip import tag_dn_ip_router
-#from api.user_channel import users_channel_router
+from apiv2.user_channel import users_channel_router
 from apiv2.users import users_router
 from apiv2.users_admin import users_admin_router
 
@@ -45,8 +45,8 @@ api_router.include_router(resolution_router, dependencies=[Depends(get_current_u
 api_router.include_router(scheduler_router, dependencies=[Depends(get_current_user), Depends(check_scheduler_role)])
 api_router.include_router(scheduler_admin_router, dependencies=[Depends(get_current_user), Depends(check_admin_role)])
 api_router.include_router(channels_router, dependencies=[Depends(get_current_user), Depends(check_admin_user_role)])
-api_router.include_router(channels_admin_router)
-#api_router.include_router(users_channel_router)
+api_router.include_router(channels_admin_router, dependencies=[Depends(get_current_user), Depends(check_admin_role)])
+api_router.include_router(users_channel_router, dependencies=[Depends(get_current_user), Depends(check_admin_user_role)])
 #api_router.include_router(tag_router)
 #api_router.include_router(tag_dn_ip_router)
 api_router.include_router(alert_router, dependencies=[Depends(get_current_user), Depends(check_admin_user_role)])
