23a0a496e5a5b2e584c0c394f4fd7513f1ffa5ad
Sebdraven <sebdraven@protonmail.com>
2024-06-24 17:52:49 +0200

    fix path

--- extras/docker/docker-entrypoint.sh
+++ extras/docker/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 if [[ "$1" = 'webserver' ]]; then
     poetry run uvicorn webserver:app --host 0.0.0.0
 elif [[ "$1" = 'create-user' ]]; then
-    python ctl/cli.py create-user "${@:2}"
+    poetry run python PassiveDNS/ctl/cli.py create-user "${@:2}"
 elif [[ "$1" = 'reset-password' ]]; then
     python ctl/cli.py reset-password "${@:2}"
 elif [[ "$1" = 'delete-user' ]]; then
